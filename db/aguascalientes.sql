422b2ae5782d173ca08847714748c755a96f2f05
HECTOR KEB <162727987+HCTORKEB@users.noreply.github.com>
2024-07-20 12:50:54 -0500

    Aguascalientes

---
+++ db/aguascalientes.sql
@@ -0,0 +1,12 @@
+INSERT INTO municipios (municipio, id_estado) VALUES
+('Aguascalientes', 1),
+('Asientos', 1),
+('Calvillo', 1),
+('Cosío', 1),
+('Jesús María', 1),
+('Pabellón de Arteaga', 1),
+('Rincón de Romos', 1),
+('San José de Gracia', 1),
+('Tepezalá', 1),
+('El Llano', 1),
+('San Francisco de los Romo', 1);
